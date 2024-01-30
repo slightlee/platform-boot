2b1740b74d7a9bf42c984b3949946ac58d9e44e7
demain <lmm_work@163.com>
2024-01-30 10:36:22 +0800

    handle warn

--- server/src/main/java/com/demain/server/PlatformServerApplication.java
+++ server/src/main/java/com/demain/server/PlatformServerApplication.java
@@ -3,6 +3,12 @@ package com.demain.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ *
+ * @author demain_lee
+ * @since 2024/1/30
+ */
 @SpringBootApplication
 public class PlatformServerApplication {
 
