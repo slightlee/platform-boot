b07888115e2b0d0e1d7721f22b3e9d6a10abf67c
demain <lmm_work@163.com>
2024-01-30 10:33:47 +0800

    mapper scan setting

---
+++ framework/mybatis/src/main/java/com/demain/framework/mybatis/config/MapperScanConfiguration.java
@@ -0,0 +1,22 @@
+package com.demain.framework.mybatis.config;
+
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * mapper scan 配置
+ *
+ * @author demain_lee
+ * @since 2024/1/30
+ */
+@Configuration
+public class MapperScanConfiguration {
+
+    @Bean
+    public MapperScannerConfigurer mapperScannerConfigurer() {
+        MapperScannerConfigurer scannerConfigurer = new MapperScannerConfigurer();
+        scannerConfigurer.setBasePackage("com.demain.server.*.mapper");
+        return scannerConfigurer;
+    }
+}
--- server/src/main/java/com/demain/server/PlatformServerApplication.java
+++ server/src/main/java/com/demain/server/PlatformServerApplication.java
@@ -1,11 +1,9 @@
 package com.demain.server;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan({"com.demain.server.*.mapper.**"})
 public class PlatformServerApplication {
 
     public static void main(String[] args) {
