e98a51cd080efec32ed2107e0b738d4972d4b226
demain_lee <lmm_work@163.com>
2024-03-04 16:37:15 +0800

    os util

---
+++ framework/core/src/main/java/com/demain/framework/core/util/OsUtil.java
@@ -0,0 +1,29 @@
+package com.demain.framework.core.util;
+
+/**
+ * 操作系统工具类
+ *
+ * @author demain_lee
+ * @since 2024/3/4
+ */
+public class OsUtil {
+
+    private static final String OS = System.getProperty("os.name").toLowerCase();
+
+    public static boolean isLinux() {
+        return OS.contains("linux");
+    }
+
+    public static boolean isMacOS() {
+        return OS.contains("mac") && OS.contains("os") && OS.contains("x");
+    }
+
+    public static boolean isMacOSX() {
+        return OS.contains("mac") && OS.contains("os") && OS.contains("x");
+    }
+
+    public static boolean isWindows() {
+        return OS.contains("windows");
+    }
+
+}
