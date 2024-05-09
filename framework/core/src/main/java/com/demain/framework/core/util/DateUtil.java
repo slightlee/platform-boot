10fc3240d961644213a0214f7d604c3ea2cffc5a
demain_lee <lmm_work@163.com>
2024-05-09 17:20:24 +0800

    date util

--- framework/core/src/main/java/com/demain/framework/core/util/DateUtil.java
+++ framework/core/src/main/java/com/demain/framework/core/util/DateUtil.java
@@ -1,9 +1,7 @@
 package com.demain.framework.core.util;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.Instant;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 /**
  * 日期处理工具类
@@ -74,6 +72,148 @@ public class DateUtil {
      */
     public static LocalDateTime currentLocalDateTime() {
         return LocalDateTime.now();
+    }
+    
+    /**
+     * 获取当前日期
+     *
+     * @return LocalDate
+     */
+    public static LocalDate currentLocalDate() {
+        return LocalDate.now();
+    }
+    
+    /**
+     * 格式化日期
+     *
+     * @param date 日期
+     * @param dateFormat 格式
+     * @return 格式化后的日期
+     */
+    public static String dateFormat(LocalDateTime date, String dateFormat) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(dateFormat);
+        return date.format(formatter);
+    }
+    
+    /**
+     * 添加秒
+     *
+     * @param date 日期
+     * @param second 秒
+     * @return 添加后的日期
+     */
+    public static LocalDateTime addSecond(LocalDateTime date, int second) {
+        return date.plusSeconds(second);
+    }
+    
+    /**
+     * 添加分钟
+     *
+     * @param date 日期
+     * @param minute 分钟
+     * @return 添加后的日期
+     */
+    public static LocalDateTime addMinute(LocalDateTime date, int minute) {
+        return date.plusMinutes(minute);
+    }
+    
+    /**
+     * 添加小时
+     *
+     * @param date 日期
+     * @param hour 小时
+     * @return 添加后的日期
+     */
+    public static LocalDateTime addHour(LocalDateTime date, int hour) {
+        return date.plusHours(hour);
+    }
+    
+    /**
+     * 添加天
+     *
+     * @param date 日期
+     * @param day 天
+     * @return 添加后的日期
+     */
+    public static LocalDateTime addDay(LocalDateTime date, int day) {
+        return date.plusDays(day);
+    }
+    
+    /**
+     * 添加月
+     *
+     * @param date 日期
+     * @param month 月
+     * @return 添加后的日期
+     */
+    public static LocalDateTime addMonth(LocalDateTime date, int month) {
+        return date.plusMonths(month);
+    }
+    
+    /**
+     * 添加年
+     *
+     * @param date 日期
+     * @param year 年
+     * @return 添加后的日期
+     */
+    public static LocalDateTime addYear(LocalDateTime date, int year) {
+        return date.plusYears(year);
+    }
+    
+    public static final String[] WEEK_DAY_OF_CHINESE = new String[]{"周日", "周一", "周二", "周三", "周四", "周五", "周六"};
+    
+    /**
+     * 日期转星期
+     *
+     * @param date 日期
+     * @return 星期
+     */
+    public static String dateToWeek(LocalDate date) {
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
+        return WEEK_DAY_OF_CHINESE[dayOfWeek.getValue() % 7];
+    }
+    
+    /**
+     * 获取一天的开始时间
+     *
+     * @param date 日期 2024-05-09T16:56:47.906073800
+     * @return 一天的开始时间 2024-05-09T00:00
+     */
+    public static LocalDateTime getStartTimeOfDay(LocalDateTime date) {
+        if (date == null) {
+            return null;
+        } else {
+            // 获取一天的开始时间，即00:00
+            return date.toLocalDate().atStartOfDay();
+        }
+    }
+    
+    /**
+     * 获取一天的结束时间
+     *
+     * @param date 日期 2024-05-09T17:00:35.878008700
+     * @return 一天的结束时间 2024-05-09T23:59:59.999999999
+     */
+    public static LocalDateTime getEndTimeOfDay(LocalDateTime date) {
+        if (date == null) {
+            return null;
+        } else {
+            // 获取一天的结束时间，即23:59:59.999999999
+            return date.toLocalDate().atTime(LocalTime.MAX);
+        }
+    }
+    
+    /**
+     * 判断当前时间是否在指定时间范围内
+     *
+     * @param nowTime 当前时间
+     * @param beginTime 开始时间
+     * @param endTime 结束时间
+     * @return true 在指定时间范围内，false 不在指定时间范围内
+     */
+    public static Boolean betweenStartAndEnd(Instant nowTime, Instant beginTime, Instant endTime) {
+        return nowTime.isAfter(beginTime) && nowTime.isBefore(endTime);
     }
     
 }
